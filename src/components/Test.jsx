bea77dfe4f0395e8f6f74bfee24fa7f5e8054e40
caizongding <caizongding@gmail.com>
2020-06-01 23:44:20 +0800

    test commit

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Test extends Component {
   render() {
-    return <div>running test component tt ff dd test</div>;
+    return <div>running</div>;
   }
 }
 
