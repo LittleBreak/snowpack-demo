37c39c8b5b54da4387dab9cf6f3a7a00d44a3a1d
caizongding <caizongding@gmail.com>
2020-06-01 23:07:59 +0800

    init commit

---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return <div>running test component</div>;
+  }
+}
+
+export default Test;
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Test from './components/Test';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>,
   document.getElementById('root'),
 );
